d22c827768e80dd945a0d3a067f1bd9eb055cce5
Probal Ranjan Paul <probal.ranjan1198@gmail.com>
2022-05-28 21:26:28 +0600

    delete product from delete fetch

--- src/Pages/Dashboard/ManageProducts/ManageProducts.js
+++ src/Pages/Dashboard/ManageProducts/ManageProducts.js
@@ -1,10 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
+import { toast } from 'react-toastify';
+import useTools from '../../../hooks/useTools/useTools';
+import Loading from '../../Shared/Loading/Loading';
 import './ManageProducts.css';
 
 const ManageProducts = () => {
+
+    const [tools, setTools] = useTools();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (tools.length > 0) {
+            setLoading(false);
+        }
+    })
+
+    const handleDelete = (id) => {
+        const proceed = window.confirm('Are you sure?')
+        if (proceed) {
+            const url = `http://localhost:5000/tools/${id}`
+            fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+                },
+            })
+                .then(res => res.json())
+                .then(() => {
+                    const remaining = tools.filter(tool => tool._id !== id)
+                    setTools(remaining);
+                    toast.success("Product Deleted")
+                    // console.log(data)
+                })
+        }
+    }
+
+    if (loading) {
+        return <Loading></Loading>
+    }
+
     return (
         <div>
             <h2>Manage Products</h2>
+            <p>Number of Product: {tools.length}</p>
+            <div>
+                <Table responsive="sm">
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Quantity</th>
+                            <th>Min. Order</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            tools?.map((tool, index) =>
+                                <tr key={tool._id}>
+                                    <td>{index + 1}</td>
+                                    <td>
+                                        <img src={tool.img} alt="" className='w-50' />
+                                    </td>
+                                    <td>{tool.name}</td>
+                                    <td>{tool.quantity}</td>
+                                    <td>{tool.minOrder}</td>
+                                    <td>${tool.price}</td>
+                                    <td>
+                                        <button onClick={() => handleDelete(tool._id)} className='accent-button'>Delete</button>
+                                    </td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </Table>
+            </div>
         </div>
     );
 };
--- src/Pages/Dashboard/MyProfile/MyProfile.js
+++ src/Pages/Dashboard/MyProfile/MyProfile.js
@@ -1,10 +1,103 @@
 import React from 'react';
+import { Form } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+import auth from '../../../firebase.init';
+import Loading from '../../Shared/Loading/Loading';
+import './MyProfile.css';
 
 const MyProfile = () => {
+
+    const [user, loading] = useAuthState(auth);
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        console.log(data)
+
+    }
+
+    if (loading) {
+        return <Loading></Loading>
+    }
+
     return (
-        <div>
-            <h2>My Profile</h2>
-            <p>ksmfvvdfxk</p>
+        <div className='dashboard-title'>
+            <h2 className=''>My Profile</h2>
+            <div className='py-4 dashboard-profile'>
+                <img src={user?.photoURL} alt="" />
+                <div>
+                    <h4 className='fw-bold'>{user?.displayName}</h4>
+                    <p className='mb-0'>{user?.email}</p>
+                </div>
+            </div>
+            <div className='login-form my-4 w-75 d-block mx-auto'>
+
+                <Form onSubmit={handleSubmit(onSubmit)}>
+
+                    <Form.Group className="mb-3" controlId="formBasicName">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control
+                            type="text"
+                            className='input-field'
+                            value={user?.displayName}
+                            {...register("name")}
+                            readOnly
+                        />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicReview">
+                        <Form.Label>Review</Form.Label>
+                        <Form.Control as="textarea" rows={3}
+                            type="text"
+                            placeholder="Add review...."
+                            className='input-field'
+                            {...register("review", {
+                                required: {
+                                    value: true,
+                                    message: 'Write minimum 10 characters'
+                                }
+                            })}
+                        />
+                        {errors?.review?.type === 'required' && <span style={{ color: "#f25c05" }}>{errors.review.message}</span>}
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicRating">
+                        <Form.Label>Rating</Form.Label>
+                        <Form.Control
+                            type="number"
+                            defaultValue={0}
+                            className='input-field'
+                            {...register("rating", {
+                                min: {
+                                    value: 1,
+                                    message: "Please add rating more than 0"
+                                },
+                                max: {
+                                    value: 5,
+                                    message: "Can't rating more than 5"
+                                },
+                                valueAsNumber: true,
+                            })}
+                        />
+                        {errors?.rating?.type === 'min' && <span style={{ color: "#f25c05" }}>{errors.rating.message}</span>}
+                        {errors?.rating?.type === 'max' && <span style={{ color: "#f25c05" }}>{errors.rating.message}</span>}
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicUserImage">
+                        <Form.Label>Your Image</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Enter image URL"
+                            className='input-field'
+                            {...register("img")}
+                            readOnly
+                        />
+                    </Form.Group>
+
+                    <button className='primary-button-lg'>Add a Review</button>
+                </Form>
+
+            </div>
         </div>
     );
 };
